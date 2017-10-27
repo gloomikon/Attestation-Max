aa4836a357472e75ee8a3972a977f53443fd143f
gloomikon <gloomikon@gmail.com>
2017-10-27 15:44:44 +0300

    +

---
+++ max_task_10.js
@@ -0,0 +1,8 @@
+ 'use strict';
+ const arr = [
+ [1,6,8,-1],
+ [0,2,-6,5],
+ [0,-5,3,6],
+ [9,-1,1,0] ];
+const diagonale = inputAray => inputAray.map((array,index) => array[index]);
+console.log(diagonale(arr));
---
+++ max_task_9.js
@@ -0,0 +1,17 @@
+'use strict';
+const Fib = n =>
+{
+	let previousN = 0;
+  let presentN = 1;
+  for (let i=0;i<n;i++)
+  {
+    let newN = previousN + presentN;
+    previousN = presentN;
+    presentN = newN;
+  }
+  return previousN;
+}
+for (let n=1;n<10;n++)
+{
+  console.log(Fib(n)); //1 1 2 3 5 8 13 21 34
+}
